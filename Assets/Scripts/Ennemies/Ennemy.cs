32f32ac596567ebb6b86ebb01b397bb226ce602e
teakguillaume@gmail.com <147896523teak>
2020-11-13 20:24:47 +0100

    First Ennemy Created
    -We have created the 1 st enemy , which moves  by teleporting and shoot with a delay

---
+++ Assets/Scripts/Ennemies/Ennemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Ennemy : Entity
+{
+    
+
+   
+   
+
+    public abstract void Move();
+    public abstract void Shoot();
+    void OnCollisionEnter(Collision collision)
+    {
+        // Debug-draw all contact points and normals
+        if (collision.rigidbody.tag == "Player") // the ennemy crashes if he collides with player
+        {
+            Destroy(this.gameObject);
+        }
+
+        if (collision.rigidbody.tag == "Bullet") // the ennemy crashes if he collides with player
+        {
+            this.loseHP(1);
+            Debug.Log("Ennemy HP : " + current_HP);
+        }
+
+        if (current_HP <= 0)  // If the player has nno HP , he dies 
+        {
+            Destroy(this.gameObject);
+        }
+
+
+        /* // Play a sound if the colliding objects had a big impact.
+         if (collision.relativeVelocity.magnitude > 2)
+             audioSource.Play();
+             */
+    }
+}
---
+++ Assets/Scripts/Ennemies/EnnemyTypeB.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemyTypeB : Ennemy
+{
+    [SerializeField]
+    private GameObject target;
+
+
+    private Camera m_MainCamera;
+    [SerializeField] private float m_verticalSpeed;
+    [SerializeField] private float m_height = Screen.height;
+
+    // Start is called before the first frame update
+    public override void Awake()
+    {
+        base.Awake();
+
+        m_MainCamera = Camera.main;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    public override void Move()
+    {
+        Vector3 screenPos = m_MainCamera.WorldToScreenPoint(this.transform.position);
+
+        //System.Console.WriteLine("balle.y = ", screenPos.y, ", position max_ecran = ", m_height);
+        if (screenPos.y < 0)
+            Destroy(gameObject, 0f);
+        if (screenPos.y > 0)
+            this.transform.position = new Vector3(transform.position.x + (m_verticalSpeed * Time.deltaTime * 0.2f), transform.position.y - (m_verticalSpeed * Time.deltaTime), transform.position.z); // Goes toward down direction
+    }
+
+    public override void Shoot()
+    {
+        
+
+    }
+}
